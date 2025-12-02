378be0011510fd141392d108adaa9ee03ba5387a
giri <vundalugiri009@gmail.com>
2025-12-02 11:17:07 +0530

    fundamnel

---
+++ code.js
@@ -0,0 +1,6 @@
+let name="giri"
+const age=21
+var city="chennai"
+console.log("name:",name)
+console.log("age:",age)
+console.log("city:",city)
--- firstday.js
+++ firstday.js
@@ -1,1 +1,19 @@
-console.log("Hello, World!");
+let a=10
+let b=20
+let sum=a+b
+console.log("sum:",sum)
+let lenght=15
+let breadth=10
+let area=lenght*breadth
+console.log("Area:",area)
+let num=11
+if(num%2==0)
+    console.log("even")
+else
+    console.log("odd")
+let firstname="giri"
+let secondname="reddy"
+console.log("fullname:",firstname+" "+secondname)
+ let x=10 
+ let y=20
+ console.log(x>y)
