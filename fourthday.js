3924d34ba7ac06ef42bc0da084f334ea95b2a2ae
giri <vundalugiri009@gmail.com>
2025-12-03 11:42:24 +0530

    ES6 concepts

---
+++ fourthday.js
@@ -0,0 +1,15 @@
+const user={
+    name:"giri",
+    age:21,
+    hobbies:["reading","playing","coding","traveling",],
+    address: {
+        City:"tiruapthi",
+        state:"andhra pradesh",
+        pincode:517588  
+
+    }
+};
+console.log("User Name:",user.name);
+console.log("User Age:",user.age);
+console.log("User Hobbies:",user.hobbies);
+console.log("User Address:",user.address);
