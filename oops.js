aabbaf8b6f17cf87371aac3fe9d5729c5e4452ae
giri <vundalugiri009@gmail.com>
2025-12-12 10:38:01 +0530

    opps

--- oops.js
+++ oops.js
@@ -1,7 +1,7 @@
-class user {
-  constructor (name, age) {
-    this.name =name;
-    this.age = age;
+class user {   
+  constructor (name,  mail) {
+    this.name = name;  
+    this.mail = mail;
   }    
 
 getdetails() 
@@ -11,3 +11,20 @@ getdetails()
 }
     const user1 =new user("giri",21);
     console.log(user1.getdetails());        
+//extend class
+class adminuser extends user {
+    constructor(name, mail, role) {
+        super (name, mail);
+        this.role = role;
+
+    }
+    getadmindetails(){
+        return `Name: ${this.name}, Mail: ${this.mail}, Role: ${this.role}`
+
+    }
+}
+// Creating objects
+const user1= new user("giri", "giri009@gmail.com",);
+console.log(user1.getdetails());        
+const admin1= new adminuser("ajay", "ajay009@gmail.com", "superadmin"); 
+console.log(admin1.getadmindetails());  
